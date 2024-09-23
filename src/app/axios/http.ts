2c40c8a7e3de1c9f2338e5bfea53bf5f4fa3177e
Maxime <maxime.eloir@agenceseize.fr>
2024-09-23 15:19:58 +0200

    Mix domain

--- src/app/axios/http.ts
+++ src/app/axios/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import https from "https";
 
 const http = axios.create({
-  baseURL: 'http://portfolio-maxime-back.maxime-eloir.fr/wp-json/portfolio/v1/',
+  baseURL: 'https://portfolio-maxime-back.maxime-eloir.fr/wp-json/portfolio/v1/',
 })
 
 export default http
